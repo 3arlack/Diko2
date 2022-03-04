8bd76ab11106428c31c65498bcc7f55d715c670b
3arlack <earlack@gmail.com>
2022-03-04 13:46:20 +0100

    Rejoindre partie

--- src/app/rejoindre-partie/rejoindre-partie.page.ts
+++ src/app/rejoindre-partie/rejoindre-partie.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonInput } from '@ionic/angular';
+import { PartieService } from '../services/partie.service';
 
 @Component({
   selector: 'app-rejoindre-partie',
   templateUrl: './rejoindre-partie.page.html',
   styleUrls: ['./rejoindre-partie.page.scss'],
 })
+
+
 export class RejoindrePartiePage implements OnInit {
-
-  constructor() { }
-
+  
+  @ViewChild(IonInput) partie:IonInput;
+  
+  constructor(private service:PartieService, private route:Router) { }
+  
   ngOnInit() {
+  }
+
+  OK(){
+    this.service.getPartie().subscribe(u=>{
+      if (u[this.partie.value]){
+        this.route.navigate(['/add-name']);
+      } else {
+        
+      }
+    })
   }
 
 }
