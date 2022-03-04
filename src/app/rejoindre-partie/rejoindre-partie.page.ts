3fa73dfd05ca6242d6ec7240f7f50dcc2c853696
3arlack <earlack@gmail.com>
2022-03-04 13:55:27 +0100

    Rejoindre partie OK

--- src/app/rejoindre-partie/rejoindre-partie.page.ts
+++ src/app/rejoindre-partie/rejoindre-partie.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonInput } from '@ionic/angular';
+import { AlertController, IonInput } from '@ionic/angular';
 import { PartieService } from '../services/partie.service';
 
 @Component({
@@ -12,21 +12,29 @@ import { PartieService } from '../services/partie.service';
 
 export class RejoindrePartiePage implements OnInit {
   
-  @ViewChild(IonInput) partie:IonInput;
+  @ViewChild("numberGame") partie:IonInput;
   
-  constructor(private service:PartieService, private route:Router) { }
+  constructor(private service:PartieService, private route:Router, private alert:AlertController) { }
   
   ngOnInit() {
   }
 
+  //Check if game exists, else displays an error message
   OK(){
+    // Gets game sessions from DB
     this.service.getPartie().subscribe(u=>{
-      if (u[this.partie.value]){
-        this.route.navigate(['/add-name']);
+      if (u[this.partie.value]){ // If game exists
+        this.route.navigate(['/add-name']); // Route to next component
       } else {
-        
+        //Displays an error message
+        this.alert.create({
+          header:"Erreur",
+          message:"Ce numéro de partie n'existe pas !",
+          buttons:['OK']
+        }).then( res=>{
+          res.present();
+        })
       }
     })
   }
-
 }
