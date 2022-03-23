c1052166d0be1b334a71d95ff37733d803b1bbf2
3arlack <earlack@gmail.com>
2022-03-23 11:34:10 +0100

    Suppression anciennes classes & services

--- services/old/ws_getParties.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *"); // permet le debug en local
-ini_set('display_errors', 1); // affiche toutes les erreurs
-error_reporting(E_ALL); // affiche toutes les erreurs
-require_once("../classes/class_parties.php");
-
-echo(parties::readAllParties());
-
-?>
