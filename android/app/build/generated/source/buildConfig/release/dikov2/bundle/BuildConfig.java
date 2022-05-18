2ee4ba0a7e03a4341533f2e63b222ea257857510
3arlack <earlack@gmail.com>
2022-05-18 14:06:39 +0200

    number... :'(

--- android/app/build/generated/source/buildConfig/release/dikov2/bundle/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/dikov2/bundle/BuildConfig.java
@@ -1,12 +1,12 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package dikov3.bundle;
+package dikov2.bundle;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "dikov3.bundle";
+  public static final String APPLICATION_ID = "dikov2.bundle";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.1";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.0";
 }
--- android/app/src/main/java/dikov3/bundle/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package dikov3.bundle;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
